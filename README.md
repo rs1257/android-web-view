d177f55a3b577ff9e72d1f542064b87bbeb08d0f
ryansmith <ryansmith300082@gmail.com>
2022-06-25 10:48:16 +0100

    Migrate files from Bitbucket

---
+++ README.md
@@ -0,0 +1,3 @@
+# Android Web View
+
+A simple example of a webview used to render a website into an android app.
---
+++ app/src/main/java/com/pascal/web/app/MainActivity.java
@@ -0,0 +1,41 @@
+package com.pascal.web.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webView);
+
+        setupWebView();
+        webView.loadUrl("https://www.google.co.uk");
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private void setupWebView() {
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+
+        WebViewClient webViewClient = new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(String.valueOf(request.getUrl()));
+                return true;
+            }
+        };
+
+        webView.setWebViewClient(webViewClient);
+    }
+}
